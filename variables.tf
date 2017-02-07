5ae7bd2bdf17938f088f1a0ba76639e8cbf5f67d
Nick Simmonds <nsimmonds@tetrascience.com>
2017-02-07 18:41:36 -0500

    Successful build

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 # ubuntu-trusty-14.04 (x64)
 variable "aws_amis" {
   default = {
-    "us-east-1" = "ami-5f709f34"
+    "us-west-2" = "ami-5f709f34"
     "us-west-2" = "ami-7f675e4f"
   }
 }
 
 variable "availability_zones" {
-  default     = "us-east-1b,us-east-1c,us-east-1d,us-east-1e"
+  default     = "us-west-2c,us-west-2a,us-west-2b"
   description = "List of availability zones, use AWS CLI to find your "
 }
 
