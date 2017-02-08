6934c5b6dd3e58ab1b5d8aa756151ccc84eb127b
Nick Simmonds <nsimmonds@tetrascience.com>
2017-02-07 19:52:51 -0500

    New AMI

--- variables.tf
+++ variables.tf
@@ -6,8 +6,8 @@ variable "aws_region" {
 # ubuntu-trusty-14.04 (x64)
 variable "aws_amis" {
   default = {
-    "us-west-2" = "ami-5f709f34"
-    "us-west-2" = "ami-7f675e4f"
+    "us-west-2" = "ami-f173cc91"
+    "us-west-2" = "ami-f173cc91"
   }
 }
 
